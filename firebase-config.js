3232edb82f9d37714e581e2e7bb95563e4e43193
John <delosssantosclarence3133@gmail.com>
2025-03-03 10:34:42 +0800

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-5MSxxahHtcoVEGlJyr7p-5SjxbmyGzo",
+  authDomain: "qrlog-36d7a.firebaseapp.com",
+  databaseURL: "https://qrlog-36d7a-default-rtdb.firebaseio.com",
+  projectId: "qrlog-36d7a",
+  storageBucket: "qrlog-36d7a.firebasestorage.app",
+  messagingSenderId: "630622697408",
+  appId: "1:630622697408:web:23f1abd83f5aeec7a2fa25",
+  measurementId: "G-TNF6NQ5D2G"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
